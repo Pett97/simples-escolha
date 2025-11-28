cbdfa8c95b722f3fae83f61281619029105edc9b
Pett97 <109447375+Pett97@users.noreply.github.com>
2025-11-27 23:51:54 -0300

    f-implementacao swagger
    * implementacao swagger
    
    * teste

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,21 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
-
 import { AuthService } from './auth.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) { }
 
   @Post('create')
+  @ApiOperation({ summary: 'Criar um novo usuário' })
   async register(@Body() createUserDto: CreateUserDto) {
     return this.authService.createUser(createUserDto);
   }
 
-  @Post('login')
+
   @HttpCode(HttpStatus.OK)
-  async login(@Body('login') login: string,@Body('password') password: string
-  ) {
-    return this.authService.login(login, password);
-  }
+  @ApiOperation({ summary: 'Login do usuário' })
+  @Post('login') @HttpCode(HttpStatus.OK) async login(@Body('login') login: string, @Body('password') password: string) { return this.authService.login(login, password); }
 }
--- src/vote/dto/create-vote.dto.ts
+++ src/vote/dto/create-vote.dto.ts
@@ -1,9 +1,18 @@
 import { IsInt, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateVoteDto {
+  @ApiProperty({
+    description: 'Hash único para validar o voto',
+    example: 'a8726c1b92f14e3b9f8a12e87d91f3c2',
+  })
+  @IsString({ message: 'hash voto é obrigatório' })
+  readonly hash: string;
 
-   @IsString({ message: "hash voto é obrigatorio" })
-   readonly hash: string;
-
-   @IsInt({ message: "numero chapa é obrigatorio" })
-   readonly numberVote: number;
+  @ApiProperty({
+    description: 'Número da chapa escolhida',
+    example: 12,
+  })
+  @IsInt({ message: 'numero chapa é obrigatório' })
+  readonly numberVote: number;
 }
