d074a893ba7e1967c5263fd0d2e0a635e5ffbdc6
Pett97 <henriquepeterson138@gmail.com>
2025-11-28 00:39:59 -0300

    apresentação

--- src/vote/vote.middleware.ts
+++ src/vote/vote.middleware.ts
@@ -3,8 +3,14 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class VoteMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    if (req.method == "POST" && !req.body.hash) {
-      return res.status(400).json({ message: "Para Votar é necessário informar o hash do vote" })
+    if (req.method === 'POST') {
+      const body = req.body || {};
+
+      if (!body.hash) {
+        return res.status(400).json({
+          message: 'Para Votar é necessário informar o hash do vote',
+        });
+      }
     }
 
     next();
